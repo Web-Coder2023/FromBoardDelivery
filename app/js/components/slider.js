da8b04ddfd15dca935c3d84683e3c9ccf1d9c5a2
Web-Coder2023 <denis.tihonkih.frontend.dumix@yandex.ru>
2024-01-09 19:26:22 +0900

    developer-desktop-ready

---
+++ app/js/components/slider.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+
+    spaceBetween: 24,
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.rewiews__arrow-right',
+        prevEl: '.rewiews__arrow-left',
+    },
+});
